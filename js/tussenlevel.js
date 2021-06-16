d32342f8dbd300b53008b9bcf60cd7d1f0d1f287
TimoWieme <1004149@hr.nl>
2021-06-16 11:44:15 +0200

    Added audio of tiger

---
+++ js/tussenlevel.js
@@ -0,0 +1,15 @@
+var audio = document.getElementById('audio');
+var playPauseBTN = document.getElementById('playPauseBTN');
+var count = 0;
+
+function playPause(){
+    if(count == 0){
+        count = 1;
+        audio.play();
+        playPauseBTN.innerHTML = "Pauze &#9208"
+    } else {
+        count = 0;
+        audio.pause();
+        playPauseBTN.innerHTML = "Speel het geluid &#9658;";
+    }
+}
